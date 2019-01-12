e50f161a5e5987e325ecc824e4d9a4d72e26e0a8
Monu Singh <smonujat@gmail.com>
2019-01-12 13:03:28 +0530

    installed ejs template engine

--- routes/shop.js
+++ routes/shop.js
@@ -4,10 +4,16 @@ const express = require('express');
 
 const rootDir = require('../util/path');
 
+const adminData = require('./admin');
+
 const router = express.Router();
 
+
 router.get('/', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  console.log(adminData.products);
+  res.render('shop',{pageTitle:'Shop',
+                     prods: adminData.products,
+                    path:'/'});
 });
 
 module.exports = router;
