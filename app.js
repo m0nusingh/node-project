59d414b74fdb376b267db3dc88dec54c4dd89246
Monu Singh <smonujat@gmail.com>
2019-02-01 16:28:24 +0530

    imnplemented mongoodse to work with mongoDB

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
+ const User = require('./models/user');
 
 const app = express();
 
@@ -16,9 +18,44 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+  User.findById('5c5422cbbcbde33a5c2098d2')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+mongoose
+  .connect('mongodb+srv://king:qxuPSPl6LQThaMbu@cluster0-4mrbf.mongodb.net/shop?retryWrites=true'
+  )
+  .then(result => {
+
+    User.findOne().then( user =>{
+      
+      if(!user)
+      {
+      const user = new User({
+        name : 'Monu Singh',
+       email: "smonujat@gmail.com",
+       cart: {
+         items: []
+       }       
+});
+user.save();
+      }
+
+
+    })
+   
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
