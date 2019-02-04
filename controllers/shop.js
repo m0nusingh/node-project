d63d5742d21e109c1cfcc230967804b2093b1425
Monu Singh <smonujat@gmail.com>
2019-02-05 02:26:42 +0530

    More features added including orderes

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,5 @@
 const Product = require('../models/product');
-
+const Order = require('../models/order');
 exports.getProducts = (req, res, next) => {
   Product.find()
     .then(products => {
@@ -44,8 +44,10 @@ exports.getIndex = (req, res, next) => {
 
 exports.getCart = (req, res, next) => {
   req.user
-    .getCart()
-    .then(products => {
+    .populate('cart.items.productId')
+    .execPopulate()
+    .then(user => {
+     const products = user.cart.items;
       res.render('shop/cart', {
         path: '/cart',
         pageTitle: 'Your Cart',
@@ -70,7 +72,7 @@ exports.postCart = (req, res, next) => {
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
   req.user
-    .deleteItemFromCart(prodId)
+    .removeFromCart(prodId)
     .then(result => {
       res.redirect('/cart');
     })
@@ -78,10 +80,26 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
-  let fetchedCart;
   req.user
-    .addOrder()
-    .then(result => {
+  .populate('cart.items.productId')
+  .execPopulate()
+  .then(user => {
+   const products = user.cart.items;
+   const order = new Order({
+
+    product : products ,
+    user : {
+           name : req.user.cart.items.map( i => {
+             return {quantity : i.quantity , product : i.productId}
+           }),
+           userId: req.user
+    },
+  
+  });
+
+  return order.save();
+  })
+  .then(result => {
       res.redirect('/orders');
     })
     .catch(err => console.log(err));
